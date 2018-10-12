ac0dcd3ffc988075a7f9ba42120f3b695e19919e
Lauren Piontek <piontekle@gmail.com>
2018-10-12 21:27:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 function takeANumber(currentLine, name) {
-  var i = 0;
-  currentLine = [];
-  for (i = 0; (i < currentLine.length); i++) {
-      currentLine.push("Welcome, " + name + ". You are number " + i+1 + " in line.");
-      return currentLine[i]
-  }
+  
 }
 
 function nowServing(currentLine) {
